f028e9af9ebfb8696dcb9ce4bb5bdcb9b456905f
MihaiBront_AlienwareA51 <al396565@uji.es>
2024-12-03 17:08:54 +0100

    deleted a comment

--- src/models/Recipe.py
+++ src/models/Recipe.py
@@ -2,36 +2,6 @@ import json
 from dataclasses import dataclass, field
 from src.models.Ingredient import Ingredient
 
-# {
-#     "id": 700339,
-#     "title": "Cinnamon Vanilla Hot Chocolate",
-#     "image": "https://img.spoonacular.com/recipes/700339-312x231.jpg",
-#     "imageType": "jpg",
-#     "usedIngredientCount": 2,
-#     "missedIngredientCount": 2,
-#     "missedIngredients": [
-#         {
-#             "id": 1012010,
-#             "amount": 0.25,
-#             "unit": "teaspoon",
-#             "unitLong": "teaspoons",
-#             "unitShort": "tsp",
-#             "aisle": "Spices and Seasonings",
-#             "name": "ground cinnamon",
-#             "original": "¼ teaspoon ground cinnamon",
-#             "originalName": "ground cinnamon",
-#             "meta": [
-#             ],
-#             "image": "https://img.spoonacular.com/ingredients_100x100/cinnamon.jpg"
-#         },
-#     ],
-#     "usedIngredients": [
-#     ],
-#     "unusedIngredients": [
-#     ],
-#     "likes": 0
-# }
-
 @dataclass
 class Recipe(object):
    SpoonRecipeID:int = field(default_factory=int)
